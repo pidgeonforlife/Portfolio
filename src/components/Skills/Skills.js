6b458bb0c31bfe2ec49954bcf42757ea75911932
Eric Choi <77644021+pidgeonforlife@users.noreply.github.com>
2022-01-25 12:29:14 -0800

    Figured out styling issues

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -9,7 +9,7 @@ function Skills() {
     <div className='skillsContainer'>
         <motion.div initial={{ x: 200, opacity: 0 }}
             transition={{ delay: 0.2}}
-            animate={{ x: 0, opacity: 1 }} className='skillsInfo'>
+            animate={{ x: 0, opacity: 1 }} className='aboutInfo'>
             <h1>Skills/Tools<span style={{color: '#94CBFF'}}>.</span></h1>
             <p>Here are some of the things I've learned<span style={{color: '#94CBFF'}}>.</span></p>
         </motion.div>
